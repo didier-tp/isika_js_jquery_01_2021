beb3a0628114efa45d2b4f9082685d95ef654cc9
didier <didier@d-defrance.fr>
2021-01-20 15:59:27 +0100

    debut product.js

---
+++ jsWeb/js/product.js
@@ -0,0 +1,16 @@
+//variables globales:
+
+var tabProduits = [
+   { code:1 , nom : "cahier" , prix: 3.5 , 
+     description : "grand cahier" , categorie : "papeterie" } ,
+  { code:2 , nom : "stylo" , prix: 1.5 , 
+     description : "stylo bille bleu" , categorie : "papeterie" } ,
+  { code:3 , nom : "pc" , prix: 567.5 , 
+     description : "pc portable 15 pouces" , categorie : "ordinateur" } 
+];
+
+//...
+
+function initialisations(){
+    //....
+}
